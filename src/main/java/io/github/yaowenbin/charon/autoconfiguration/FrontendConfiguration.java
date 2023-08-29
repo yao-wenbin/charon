463734bf1b5d6b7220de8c1e073fbf295b210959
yao-wenbin <ywb992134@163.com>
2023-08-29 14:29:20 +0800

    feat: integrate frontend into spring boot.

---
+++ src/main/java/io/github/yaowenbin/charon/autoconfiguration/FrontendConfiguration.java
@@ -0,0 +1,32 @@
+package io.github.yaowenbin.charon.autoconfiguration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class FrontendConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Controller
+    static class IndexController {
+        // 系统首页
+        @GetMapping(value = { "/", "/index", "/login" })
+        public String index()
+        {
+            return "index.html";
+        }
+    }
+
+}
