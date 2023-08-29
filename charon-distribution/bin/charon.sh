84e5827080199f7d00b49004db0ea97043d31b61
yao-wenbin <ywb992134@163.com>
2023-08-29 22:28:22 +0800

    build: integrate frontend and backend by assembly plugin in distribution module.

--- charon-distribution/bin/charon.sh
+++ charon-distribution/bin/charon.sh
@@ -3,4 +3,4 @@ PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 
 SPRING_ARG="--spring.config.additional-location=file:${PARENT_DIR}/config/charon.yml"
 
-java -jar ${PARENT_DIR}/charon-server-0.0.1-SNAPHOST.jar ${SPRING_ARG}
+java -jar ${PARENT_DIR}/charon-server-0.0.1-SNAPSHOT.jar ${SPRING_ARG}
