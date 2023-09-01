01ab5f171bc6fbbd0a2d53b5f8b60b18bbe32f63
yao-wenbin <ywb992134@163.com>
2023-09-01 16:03:21 +0800

    feat: /datasources and /list api.

--- charon-server/src/main/java/io/github/yaowenbin/server/datasource/DataSourceApi.java
+++ charon-server/src/main/java/io/github/yaowenbin/server/datasource/DataSourceApi.java
@@ -1,5 +1,9 @@
 package io.github.yaowenbin.server.datasource;
 
+import io.github.yaowenbin.core.http.R;
+import io.github.yaowenbin.server.autoconfiguration.properties.DataSourceConfigurationProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,6 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2023/8/21
  */
 @RestController
-public class DataSourceApi {
+@RequiredArgsConstructor
+public class DataSourceApi{
+
+    private final DataSourceConfigurationProperties dataSourceProperties;
+
+    @GetMapping("/datasources")
+    public R<DataSourceConfigurationProperties> listApi() {
+        return R.success(dataSourceProperties);
+    }
 
 }
---
+++ charon-server/src/main/java/io/github/yaowenbin/server/monitor/MonitorApi.java
@@ -0,0 +1,24 @@
+package io.github.yaowenbin.server.monitor;
+
+import io.github.yaowenbin.commons.map.Maps;
+import io.github.yaowenbin.core.http.R;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+public class MonitorApi {
+
+    private final MonitorService monitorService;
+
+    @GetMapping("/list")
+    public R<Map> listApi() {
+        Map<Object, Object> data = Maps.builder().put("page", 1).put("size", 10).put("pageCount", 10).put("itemCount", 99).put("list", monitorService.listUnindexedSql()).build();
+        return R.success(data);
+    }
+
+}
