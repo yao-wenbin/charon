bbeaf8ffb9308ef1fbb051c088da546c4de7a4f1
yao-wenbin <ywb992134@163.com>
2023-09-06 17:53:44 +0800

    refactor ui-dashboard: delete unused page.

--- charon-ui/src/router/modules/dashboard.ts
+++ charon-ui/src/router/modules/dashboard.ts
@@ -19,43 +19,21 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/dashboard',
     name: routeName,
-    redirect: '/dashboard/console',
+    redirect: '/dashboard/workplace',
     component: Layout,
     meta: {
       title: 'Dashboard',
       icon: renderIcon(DashboardOutlined),
-      permissions: ['dashboard_console', 'dashboard_console', 'dashboard_workplace'],
       sort: 0,
     },
     children: [
       {
-        path: 'console',
-        name: `${routeName}_console`,
+        path: 'index',
+        name: `${routeName}_index`,
         meta: {
           title: '主控台',
-          permissions: ['dashboard_console'],
-          affix: true,
         },
-        component: () => import('@/views/dashboard/console/console.vue'),
-      },
-      // {
-      //   path: 'monitor',
-      //   name: `${ routeName }_monitor`,
-      //   meta: {
-      //     title: '监控页',
-      //     permissions: ['dashboard_monitor']
-      //   },
-      //   component: () => import('@/views/dashboard/monitor/monitor.vue')
-      // },
-      {
-        path: 'workplace',
-        name: `${routeName}_workplace`,
-        meta: {
-          title: '工作台',
-          keepAlive: true,
-          permissions: ['dashboard_workplace'],
-        },
-        component: () => import('@/views/dashboard/workplace/workplace.vue'),
+        component: () => import('@/views/dashboard/index.vue'),
       },
     ],
   },
