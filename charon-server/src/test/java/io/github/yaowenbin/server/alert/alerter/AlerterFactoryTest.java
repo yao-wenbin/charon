33404b9d9917cd8bce59b5f3e0a6f28ceca0b973
yao-wenbin <ywb992134@163.com>
2023-09-26 22:16:51 +0800

    test alerter: add AlerterFactoryTest

---
+++ charon-server/src/test/java/io/github/yaowenbin/server/alert/alerter/AlerterFactoryTest.java
@@ -0,0 +1,34 @@
+package io.github.yaowenbin.server.alert.alerter;
+
+import io.github.yaowenbin.server.UnitTest;
+import io.github.yaowenbin.server.alert.config.AlerterProperties;
+import io.github.yaowenbin.server.core.LoadConfigurationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class AlerterFactoryTest extends UnitTest {
+
+    @Test
+    void build_shouldThrowException_whenTypeIsUnsupported() {
+        var property = new AlerterProperties().setType("slack").setWebhook("anything");
+        assertThrows(LoadConfigurationException.class, () -> {
+            AlerterFactory.build(property);
+        });
+    }
+
+    @Test
+    void build_feishuAlerter() {
+        var property = new AlerterProperties().setType("feishu").setWebhook("some feishu webhook");
+        Alerter alerter = AlerterFactory.build(property);
+        assertThat(alerter).isInstanceOf(FeishuAlerter.class);
+    }
+
+    @Test
+    void build_weixinAlerter() {
+        var property = new AlerterProperties().setType("weixin").setWebhook("some weixin webhook");
+        Alerter alerter = AlerterFactory.build(property);
+        assertThat(alerter).isInstanceOf(WeixinAlerter.class);
+    }
+
+}
